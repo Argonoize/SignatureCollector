3e9364c1ae7aa78b7952fa442b2addd389dab31e
Argonoize <smallalien@web.de>
2022-08-09 00:01:34 +0200

    Update MainList.vue
    format

--- src/components/MainList.vue
+++ src/components/MainList.vue
@@ -52,10 +52,9 @@
                 </v-col>
                 <color-input class="customcolor" v-model="color" @change="colorPickEnd()" ref="colorInput" format="hex8" disable-alpha="true" disable-text-inputs="true"/>
             </v-row>
+
             <v-row class="d-flex justify-center align-baseline py-3" style="gap: 1rem">
 
-                
-                
                 <v-btn
                 style="background-color: #e3eaff"
                 prepend-icon="mdi-cloud-upload"
@@ -113,61 +112,61 @@ export default {
         })
     },
     methods: {
-    addRow() {
-        this.inputs.push({
-            name: '',
-            signature: ''
-        })
-    },
-    deleteRow(index) {
-      this.inputs.splice(index,1)
-    },
-    edit(index){
-        if(document.getElementById('sigDiv_'+index).style.display === "none"){
-            document.getElementById('sigDiv_'+index).style.display = "flex";
-        }
-    },
-    undo(ref) {
-        this.$refs[ref][0].undoSignature();
-    },
-    save(ref,index) {
-        const { isEmpty, data } = this.$refs[ref][0].saveSignature();
-        if(isEmpty) {
-            toast.error("Unterschrift ist leer");
-        } else if(this.inputs[index].name == "") {
-            toast.error("Name ist leer");
-        }else {
-            var base64result = data.split(',')[1];
-            this.inputs[index].signature = base64result;
-            document.getElementById('sigDiv_'+index).style.display = "none";
-            toast.success("Unterschrift gespeichert für " + this.inputs[index].name );
-        }
-    },
-    clear(ref, index) {
-        this.$refs[ref][0].clearSignature();
-    },
-    exportAsJSON() {
-        for (let index = 0; index < this.inputs.length; index++) {
-            const element = this.inputs[index];
-            if(element.name == ""){
+        addRow() {
+            this.inputs.push({
+                name: '',
+                signature: ''
+            })
+        },
+        deleteRow(index) {
+            this.inputs.splice(index,1)
+        },
+        edit(index){
+            if(document.getElementById('sigDiv_'+index).style.display === "none"){
+                document.getElementById('sigDiv_'+index).style.display = "flex";
+            }
+        },
+        undo(ref) {
+            this.$refs[ref][0].undoSignature();
+        },
+        save(ref,index) {
+            const { isEmpty, data } = this.$refs[ref][0].saveSignature();
+            if(isEmpty) {
+                toast.error("Unterschrift ist leer");
+            } else if(this.inputs[index].name == "") {
                 toast.error("Name ist leer");
-                return;
+            }else {
+                var base64result = data.split(',')[1];
+                this.inputs[index].signature = base64result;
+                document.getElementById('sigDiv_'+index).style.display = "none";
+                toast.success("Unterschrift gespeichert für " + this.inputs[index].name );
             }
-            if(document.getElementById('sigDiv_'+index).style.display !== "none"){
-                toast.error("Bitte die Unterschrift/n speichern");
-                return;
+        },
+        clear(ref, index) {
+            this.$refs[ref][0].clearSignature();
+        },
+        exportAsJSON() {
+            for (let index = 0; index < this.inputs.length; index++) {
+                const element = this.inputs[index];
+                if(element.name == ""){
+                    toast.error("Name ist leer");
+                    return;
+                }
+                if(document.getElementById('sigDiv_'+index).style.display !== "none"){
+                    toast.error("Bitte die Unterschrift/n speichern");
+                    return;
+                }
             }
-        }
-        new DataDownloader(this.inputs).download();
-    },
-    colorPickEnd() {
-        const colorInput = this.$refs.colorInput
+            new DataDownloader(this.inputs).download();
+        },
+        colorPickEnd() {
+            const colorInput = this.$refs.colorInput
 
-        this.options = {
-            penColor: "#"+colorInput.color.toHex(),
+            this.options = {
+                penColor: "#"+colorInput.color.toHex(),
+            }
         }
     }
-  }
 }
 </script>
 
